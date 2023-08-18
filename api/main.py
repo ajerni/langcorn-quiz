22a14def03db05539cdb9018ba8fa23c3dedc578
„Andi <ajerni@gmail.com>
2023-08-18 14:47:12 +0200

    done

--- api/main.py
+++ api/main.py
@@ -3,6 +3,8 @@
 # https://www.youtube.com/watch?v=iFvCZD4iS2w&t=2s
 # https://github.com/msoedov/langcorn/
 
+# langcorn kreiert automatisch eine FastAPI aus einer langchain chain:
+
 from langcorn import create_service
 
 app = create_service(
